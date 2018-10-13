025ac81ca57f731904d187d2be42437c97ee216f
Robert Forster <robert.forster+lost@beamvex.co.uk>
2018-10-13 21:08:00 +0100

    babel

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    babel: {
+      options: {
+        plugins: ['transform-react-jsx'],
+        presets: ['es2015', 'react']
+      },
+      jsx: {
+        files: [{
+          expand: true,
+          cwd: 'source/js/jsx/', // Custom folder
+          src: ['*.jsx'],
+          dest: 'source/js/jsx-compiled/', // Custom folder
+          ext: '.js'
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('default', ['babel']);
+
+}
